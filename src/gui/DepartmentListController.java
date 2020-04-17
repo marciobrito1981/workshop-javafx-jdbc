d825ed1bdc3a06a229f90bbe91a2f350a9fe91cf
Marcio Brito <marcio.brito@outlook.com>
2020-04-17 12:22:36 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.sevices.DepartmentService;
@@ -40,8 +49,9 @@ public class DepartmentListController implements Initializable {
 	
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("clicado no botao novo");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	//Injetando dependencia da classe Department Service
@@ -73,6 +83,25 @@ public class DepartmentListController implements Initializable {
 		List<Department>  list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName)); //carregar a tela
+			Pane pane = loader.load();
+			
+			//carrega a tela de cadastro de Departamento (modal)
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Entre com os dados do Departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false); //não permite que a janela seja redimensionada
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);//define se será modal ou outro tipo 
+			dialogStage.showAndWait();
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view",e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
